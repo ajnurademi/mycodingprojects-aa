fa38dfa2c81a6e55ceb3973d0d91703f7cf6226f
ajnur.adm <ajnur.ademi@hotmail.com>
2024-10-09 18:08:59 +0200

    added essential configs and error fixing

--- myWeatherApp/myweatherapp/app/page.js
+++ myWeatherApp/myweatherapp/app/page.js
@@ -2,7 +2,9 @@
 export default function Home() {
   return (
     <>
-    
+    <div className="bg-blue-500 text-white p-4">
+      <h1 className="text-lg font-bold">Willkommen in meiner Wetter-App</h1>
+    </div>
     </>
   );
 }
--- myWeatherApp/myweatherapp/tailwind.config.js
+++ myWeatherApp/myweatherapp/tailwind.config.js
@@ -1,17 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './app/**/*.{js,ts,jsx,tsx}', 
+    './components/**/*.{js,ts,jsx,tsx}', 
+    './pages/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
     },
+    colors: {
+      'blue': '#1fb6ff',
+      'gray-light': '#d3dce6',
+    },
+    extend: {},
   },
   plugins: [],
-};
+}
