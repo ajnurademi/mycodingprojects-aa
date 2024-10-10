ef23f2962aaa3102dba7885ecef3e17712dbbc87
ajnur.adm <ajnur.ademi@hotmail.com>
2024-10-10 20:31:34 +0200

    finished hero component && started nextDay component && added icons

--- myWeatherApp/myweatherapp/app/page.js
+++ myWeatherApp/myweatherapp/app/page.js
@@ -4,14 +4,12 @@ import { NextDays } from '../components/nextDays';
 
 export default function Home() {
   return (
-    <>
-      <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-blue-500 to-indigo-600">
-        <div className="md:w-[40vh] md:h-[60vh] lg:w-[60vh] lg:h-[80vh] flex flex-col items-center justify-center border-dashed border-4 border-white bg-white/10 backdrop-blur-lg rounded-[3vh]">
-          <Header />
-          <Hero />
-          <NextDays />
-        </div>
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-blue-500 to-indigo-600">
+      <div className="md:w-[40vh] md:h-[60vh] lg:w-[60vh] lg:h-[80vh] flex flex-col items-center justify-center border-dashed border-4 border-white bg-white/10 backdrop-blur-lg rounded-[3vh]">
+        <Header />
+        <Hero />
+        <NextDays />
       </div>
-    </>
+    </div>
   );
 }
